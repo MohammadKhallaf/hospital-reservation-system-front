15967648173275955b9d1f0dec486ad7e079adbe
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-04-18 03:14:47 +0200

    Update specialization gallery

--- src/components/SpecGallery/SpecGallery.jsx
+++ src/components/SpecGallery/SpecGallery.jsx
@@ -1,44 +1,50 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
-import card from "../../assets/dentist.jpg";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import "./SpecGallery.scss";
+
 const SpecGallery = () => {
   const [gallery, setGallery] = useState([]);
-  const [initial, setInitial] = useState(true);
+
   useEffect(() => {
-    if (initial) {
-      axios
-        .get("http://localhost:8000/api/doctors/specializations")
-        .then((e) => {
-          setGallery(e.data);
-        })
-        .catch((e) => console.log(e));
-      setInitial(false);
-    }
+    axios
+      .get("api/doctors/specializations/")
+      .then((e) => {
+        setGallery(e.data);
+      })
+      .catch((e) => console.log(e));
 
     return () => {};
-  }, [gallery]);
+  }, []);
 
   return (
     <Container className="py-3">
-      <h1>Specializations</h1>
-      <Row xs={1} md={3} className="g-3">
+      <h1 className="py-3">Specializations</h1>
+      <Row xs={1} sm={2} md={3} className="g-3">
         {gallery?.map((item, index, array) => (
-          <Col key={`card-${index}`}>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={card} />
-              <Card.ImgOverlay>
-                <Card.Title>{item.name}</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Link to={`/list/${item.id}`}>
-                  <Button variant="primary">Go somewhere</Button>
-                </Link>
-              </Card.ImgOverlay>
-            </Card>
+          <Col key={`card-${index}`} className="spec-card">
+            <Link
+              to={`/list/${item.id}`}
+              className="text-black text-decoration-none"
+            >
+              <Card className="border-0 border-start bg-secondary bg-gradient">
+                <Card.Img
+                  variant="top"
+          
+                  width="100%"
+                  src={process.env.PUBLIC_URL + item.picture}
+                  className="img-fluid float-start"
+                  onError={(current) => {
+                    current.target.src = require("../../assets/fallback/specialization.jpg");
+                  }}
+                  alt={`${item.name} specialization`}
+                />
+                <Card.Body>
+                  <Card.Title>{item.name}</Card.Title>
+                </Card.Body>
+              </Card>
+            </Link>
           </Col>
         ))}
       </Row>
