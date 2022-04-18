657c83bbe0c362c65637315ca1be880dd58f9bc2
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-04-18 03:13:36 +0200

    Update doctor list page

--- src/pages/DoctorsList/DoctorsList.jsx
+++ src/pages/DoctorsList/DoctorsList.jsx
@@ -1,64 +1,89 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Button, Card, Col, Container, Image, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-import { FaDollarSign } from "react-icons/fa";
+import AppointmentModel from "../../components/AppointmentModal/AppointmentModel";
+import axios from "axios";
+
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { FaDollarSign, FaRegClock } from "react-icons/fa";
+import { FcClock, FcMoneyTransfer } from "react-icons/fc";
+import "./DoctorList.scss";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+
 const DoctorsList = () => {
+  const Dayjs = dayjs;
+  Dayjs.extend(customParseFormat);
+  const [modalState, setModalState] = useState({ show: false, data: {} });
   const params = useParams();
   const [doctorList, setDoctorList] = useState([]);
+  const [header, setHeader] = useState("No Doctors Available Yet");
 
   useEffect(() => {
-    axios
-      .get("http://localhost:8000/api/doctors/specializations/" + params.listId)
-      .then((e) => setDoctorList(e.data));
-  }, []);
+    axios.get("api/doctors/specializations/" + params.listId).then((e) => {
+      setDoctorList(e.data);
+      setHeader(e.data[0].specialization.name);
+    });
+  }, [params.listId]);
 
   return (
     <Container className="py-5">
-      <h1>DoctorsList</h1>
-      <Row className="g-3">
+      <AppointmentModel
+        data={modalState.data}
+        show={modalState.show}
+        onHide={() => setModalState({ ...modalState, show: false })}
+      />
+
+      <h1>{header}</h1>
+      <Row className="gap-3 p-3">
         {doctorList?.map((item, index, array) => (
-          <Col xs={12} key={`doctor-${index}`} className="border" height="15rem">
-            <Row>
-              <Col xd={12} md={4}>
-                <img
-                  //!
-                  //src={item.picture}
-                  src={require("../../assets/dentist.jpg")}
-                  className="img-fluid img-thumbnail float-start"
-                  onError={(current) => {
-                    current.target.src = require("../../assets/dentist.jpg");
-                  }}
-                  alt=""
-                  style={{
-                    objectFit: "cover",
-                  }}
-                />
-              </Col>
+          <Col
+            xs={12}
+            sm={11}
+            key={`doctor-${index}`}
+            className="row doctor-card border p-3"
+            style={{ minHeight: "10rem" }}
+          >
+            <Col className="d-flex justify-content-center">
+              <img
+                src={process.env.PUBLIC_URL + item.picture}
+                className="img-fluid float-start"
+                onError={(current) => {
+                  current.target.src =
+                    item.gender === "female"
+                      ? require("../../assets/fallback/female-doctor.jpg")
+                      : require("../../assets/fallback/male-doctor.jpg");
+                }}
+                alt="doctor"
+              />
+            </Col>
 
-              <Col xd={12} md={5}>
-                <h2>{item.name}</h2>
-                <p className="fs-4">
-                  <FaDollarSign />
-                  {item.price} LE
-                </p>
-
-                <p className="fs-4">{item.experince}</p>
-                <Button>Book</Button>
-              </Col>
-              <Col xd={12} md={3} style={{height:"100%",overFlow:"hidden",}}>
-                {item.doctor_schedules.map((item) => (
-                  <Row className="py-3">
-                    <Button>
-                      <p>{item.day}</p>
-                      <p>{item.time_from}</p>
-                      <p>{item.time_to}</p>
-                    </Button>
-                  </Row>
-                  
-                ))}
-              </Col>
-            </Row>
+            <Col xs={12} md={5} className="p-3">
+              <h2 className="fw-bolder">{item.name}</h2>
+              <h3>{item.specialization.name}</h3>
+              <p className="fs-5 lead text-muted">{item.experience}</p>
+              <p className="fs-5 d-flex flex-row align-items-center text-success">
+                <FcMoneyTransfer className="me-3 align-baseline" />
+                {item.fees} LE
+              </p>
+              <time className="fs-6 d-flex flex-row align-items-center">
+                <FcClock className="me-3" />
+                {Dayjs(item.arrival_time, "HH:mm:ss").format("hh:mm A")}
+              </time>
+            </Col>
+            <Col
+              xd={12}
+              md={3}
+              // style={{ height: "100%", overFlow: "hidden" }}
+              className=" d-flex align-items-end justify-content-end p-3"
+            >
+              <Button
+                variant="primary"
+                className="btn-lg"
+                onClick={() => setModalState({ show: true, data: item })}
+              >
+                Book
+              </Button>
+            </Col>
           </Col>
         ))}
       </Row>
