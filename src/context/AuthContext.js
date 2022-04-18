9b252bc4c2ddcc059f0da9eda9baf8c251ce5c14
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-04-18 03:00:23 +0200

    Add toasting action

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 import { createContext, useState, useEffect, useCallback } from "react";
 import jwt_decode from "jwt-decode";
+import Toastify from "toastify-js";
+import { useNavigate } from "react-router-dom";
 const AuthContext = createContext();
 
 export default AuthContext;
 
 export const AuthProvider = ({ children }) => {
+  const navigate = useNavigate();
   const [initial, setInitial] = useState(true);
   const [authTokens, setAuthTokens] = useState(() =>
     localStorage.getItem("authTokens")
@@ -26,18 +29,36 @@ export const AuthProvider = ({ children }) => {
         setAuthTokens(res.data);
         setUser(jwt_decode(res.data.access));
         localStorage.setItem("authTokens", JSON.stringify(res.data));
+        Toastify({
+          text: "Logged In Successfully",
+          duration: 3000,
+        }).showToast();
+        navigate(-1);
       })
-      .catch((error) => {});
+      .catch((error) => {
+        Toastify({
+          text: "An error occured",
+          duration: 3000,
+          style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+          },
+        }).showToast();
+      });
   };
   const logoutUser = () => {
     setAuthTokens(null);
     setUser(null);
     localStorage.removeItem("authTokens");
+    Toastify({
+      text: "Goodbye",
+
+      duration: 3000,
+    }).showToast();
   };
 
   const updateToken = useCallback(() => {
     if (!authTokens) return;
-    console.log("update")
+    console.log("update");
     axios
       .post("api/token/refresh/", {
         refresh: authTokens?.refresh,
@@ -53,18 +74,18 @@ export const AuthProvider = ({ children }) => {
     if (initial) {
       setInitial(false);
     }
-  }, []);
+  }, [authTokens, initial]);
 
   useEffect(() => {
     if (initial) updateToken();
-    const refreshInterval = 2_000; // 4 minutes
+    const refreshInterval = 240_000; // 4 minutes
     const interval = setInterval(() => {
       if (authTokens) {
-        // updateToken();
+        updateToken();
       }
     }, refreshInterval);
     return () => clearInterval(interval);
-  }, [authTokens, initial]);
+  }, [authTokens, initial, updateToken]);
 
   const contextData = {
     user,
