c784539d331a3e2ca2ff2efd703557c170e0cf2b
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-04-18 02:58:18 +0200

    Add private route

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import SiteFooter from "./components/SiteFooter/SiteFooter";
 import SiteNav from "./components/Sitenav/SiteNav";
 import { AuthProvider } from "./context/AuthContext";
 import "./index.scss";
+import AdminBoard from "./pages/AdminBoard/AdminBoard";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import DoctorsList from "./pages/DoctorsList/DoctorsList";
 import Home from "./pages/Home/Home";
+import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
-
   useEffect(() => {
     document.getElementById("loader").style.opacity = "0";
   }, []);
@@ -20,12 +21,25 @@ function App() {
       <Router>
         <AuthProvider>
           <SiteNav />
-          <Container fluid style={{ minHeight: "calc(100vh - 20rem)" }} className="p-0">
+          <Container
+            fluid
+            style={{ minHeight: "calc(100vh - 20rem)" }}
+            className="p-0"
+          >
             <Routes>
               <Route index element={<Home />} />
               <Route path="list/:listId" element={<DoctorsList />} />
+
               <Route path="login" element={<Login />} />
               <Route path="register" element={<Register />} />
+              <Route
+                path="hospitalAdmin"
+                element={
+                  <PrivateRoute>
+                    <AdminBoard />
+                  </PrivateRoute>
+                }
+              />
             </Routes>
           </Container>
         </AuthProvider>
